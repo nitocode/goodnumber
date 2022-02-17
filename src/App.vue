7032aa1a0fa0bdcb90a6db92d4a729fb9571e9ce
nitocode <lu.nitoumbi@gmail.com>
2022-02-17 22:53:14 +0100

    WIP: Improve UI

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,22 @@ import "@/assets/base.css";
 </script>
 
 <template>
-  <div>
-    <h1>NUMDLE</h1>
+  <div class="app-container text-white h-screen px-4 overflow-hidden">
+    <div class="max-w-[500px] mx-auto">
+      <header class="flex justify-between items-center mx-4 py-8">
+        <h1 class="text-2xl">NUMDLE</h1>
+        <div class="flex justify-around items-center w-[100px]">
+          <p>Stat</p>
+          <p>Git</p>
+        </div>
+      </header>
+      <RouterView />
+    </div>
   </div>
-  <RouterView />
 </template>
 
 <style>
-/* @import "@/assets/base.css"; */
-/* @tailwind base;
-@tailwind components;
-@tailwind utilities; */
+.app-container {
+  background-color: #3d4451;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
 import { createPinia } from "pinia";
 import { ref } from "vue";
 const gap = ref(0);
-const attemptLimit = ref(4);
 const userAttempt = ref(0);
 const answerList = ref([]);
 const answer = ref();
@@ -10,19 +9,20 @@ const hasFound = ref(false);
 const gameOver = ref(false);
 const question = ref({
   id: 1,
-  title: "Date de création de twitch ?",
+  title: "Date de création de twitch&nbsp;?",
   answer: 2011,
   linkForMore: "https://fr.wikipedia.org/wiki/Twitch",
+  attemptLimit: 4,
 });
 
 const addAnswerToAttemptList = (answer) => {
-  let gap = "=";
+  let gap = "✅";
   if (question.value.answer - answer < 0) {
-    gap = "-";
+    gap = "🔻";
   } else if (question.value.answer - answer > 0) {
-    gap = "+";
+    gap = "🔺";
   } else {
-    gap = "=";
+    gap = "✅";
   }
   answerList.value.push({
     answer: answer,
@@ -31,31 +31,33 @@ const addAnswerToAttemptList = (answer) => {
 };
 
 const validate = () => {
-  userAttempt.value += 1;
-  gap.value = question.value.answer - answer.value;
+  if (answer.value) {
+    userAttempt.value += 1;
+    gap.value = question.value.answer - answer.value;
 
-  if (gap.value === 0) {
-    hasFound.value = true;
-    gameOver.value = true;
-  } else {
-    if (userAttempt.value === attemptLimit.value) {
-      hasFound.value = false;
+    if (gap.value === 0) {
+      hasFound.value = true;
       gameOver.value = true;
+    } else {
+      if (userAttempt.value === question.value.attemptLimit) {
+        hasFound.value = false;
+        gameOver.value = true;
+      }
     }
+    addAnswerToAttemptList(answer.value);
   }
-  addAnswerToAttemptList(answer.value);
 };
 
 const share = () => {
   let textToShare = `Good number #${question.value.id}: ${
-    userAttempt.value === attemptLimit.value ? "😞" : userAttempt.value
-  }/${attemptLimit.value} \r\n\r\n`;
+    userAttempt.value === question.value.attemptLimit ? "😞" : userAttempt.value
+  }/${question.value.attemptLimit} \r\n\r\n`;
 
   answerList.value.forEach((a, index) => {
     textToShare += `Tentative n°${index + 1} : ${a.gap}\r\n`;
   });
 
-  textToShare += "\r\ngoodnumber.nitocode.com";
+  textToShare += "\r\nnumdle.nitocode.com";
 
   navigator.clipboard.writeText(textToShare).then(
     function () {
@@ -70,52 +72,70 @@ const share = () => {
 
 <template>
   <main class="text-center">
-    <h2>{{ question.title }}</h2>
     <div v-if="!hasFound && !gameOver">
-      <div v-for="(answerObj, index) in answerList" :key="index">
-        Tentative n°{{ index + 1 }}: {{ answerObj.answer }} :
-        {{ answerObj.gap }}
+      <div class="h-[50vh]">
+        <div class="my-10">
+          <h2 class="text-3xl" v-html="question.title"></h2>
+          <p class="mt-2">
+            (Tentative restante: {{ question.attemptLimit - userAttempt }})
+          </p>
+        </div>
+        <div v-for="(answerObj, index) in answerList" :key="index">
+          <p class="text-xl lg:text-lg my-4">
+            Tentative n°{{ index + 1 }}: {{ answerObj.answer }} :
+            {{ answerObj.gap }}
+          </p>
+        </div>
       </div>
       <div>
-        <input v-model="answer" type="number" />
+        <input
+          class="text-black px-4 py-2 text-xl"
+          v-model="answer"
+          type="number"
+        />
       </div>
       <br />
-      <button @click="validate()">VALIDER</button>
+      <button class="btn-numdle" @click="validate()">VALIDER</button>
     </div>
     <div v-else>
       <div v-if="hasFound">
-        <p>BRAVO !!!</p>
+        <p class="text-3xl my-10">BRAVO&nbsp;!!!</p>
         <p>
           Vous avez trouvé la bonne réponse en
           {{ answerList.length }} tentative{{
             answerList.length > 1 ? "s" : ""
-          }}
-          !
+          }}&nbsp;!
         </p>
       </div>
       <div v-else>
-        <p>Dommage !!!</p>
+        <p class="text-3xl my-10">Dommage&nbsp;!!!</p>
         <p>La bonne réponse est : {{ question.answer }}</p>
       </div>
 
+      <p class="text-xl my-4">Résumé</p>
       <div v-for="(answerObj, index) in answerList" :key="index">
-        Tentative n°{{ index + 1 }}: {{ answerObj.answer }} :
-        {{ answerObj.gap }}
+        <p class="text-xl my-4">
+          Tentative n°{{ index + 1 }}: {{ answerObj.answer }} :
+          {{ answerObj.gap }}
+        </p>
+      </div>
+
+      <div class="my-8">
+        <button class="btn-numdle" @click="share()">
+          Partager mon résultat
+        </button>
       </div>
 
       <div v-if="question.linkForMore">
+        <p class="text-xl mb-2">En savoir plus</p>
         <p>
-          En savoir plus :
-          <a :href="question.linkForMore" target="_blank">{{
+          <a class="underline" :href="question.linkForMore" target="_blank">{{
             question.linkForMore
           }}</a>
         </p>
       </div>
-
-      <p>Partager ma réponse à mes amis:</p>
-      <button @click="share()">Partage</button>
     </div>
   </main>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
